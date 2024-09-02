bf16f491652dc81799fc5debf5baff7a4ce1b1f9
Pankaj Kumar <64430912+Pankaj2612@users.noreply.github.com>
2024-09-02 12:08:57 +0530

    changed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -33,16 +33,11 @@ export default function App() {
       <header>
         <nav className="border-gray-200 px-4 lg:px-6 py-2.5 bg-gradient-to-r from-[#833ab4] via-[#fd1d1d] to-[#fcb045]">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="https://flowbite.com" className="flex items-center">
-              <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                className="mr-3 h-6 sm:h-9"
-                alt="Flowbite Logo"
-              />
+            <div  className="flex items-center">
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                Flowbite
+                InstaPL
               </span>
-            </a>
+            </div>
             <div className="flex items-center lg:order-2">
               <a
                 href="#"
